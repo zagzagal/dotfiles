0bf749faaa9e5e78ce367390568bff3eb4934088
Paul Schuster <zagzagal@gmail.com>
2020-05-12 14:50:52 -0400

    fixed update count

--- polybar/.config/polybar/updates-arch.fish
+++ polybar/.config/polybar/updates-arch.fish
@@ -1,13 +1,14 @@
 #! /usr/bin/env fish
 
 set -l updates (checkupdates && yay -Qum 2> /dev/null | wc -l)
+#set -l updates (checkupdates 2> /dev/null | wc -l)
 
 if test $status -ne 0
   echo ""
 else
   if test $updates -ne 0
-    set -l num (count $updates)
-    echo "$num updates"
+    #set -l num (count $updates)
+    echo "$updates updates"
   else
     echo ""
   end
